ac5a88eccc8d26779da01d35b69038563e9e71f0
Valentin <valentin.anchev@tick42.com>
2024-07-23 18:17:08 +0300

    browser platform configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IOConnectStore } from '@interopio/ng';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular17';
+
+  constructor(
+    private readonly ioConnectStore: IOConnectStore
+  ) {
+
+    console.log(this.ioConnectStore.getIOConnect())
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+import { IOConnectNg } from '@interopio/ng';
 
 import { routes } from './app.routes';
 
+import browserConfig from './config.json';
+import IOBrowserPlatform, { IOConnectBrowserPlatform } from '@interopio/browser-platform';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      IOConnectNg.forRoot({
+        browserPlatform: {
+          factory: () => {
+            return IOBrowserPlatform({
+              ...browserConfig as IOConnectBrowserPlatform.Config,
+              serviceWorker: { url: './service-worker.js' }
+            });
+          }
+        }
+      })
+    )
+  ]
 };
